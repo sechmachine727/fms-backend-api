6a5d81926de355f275c12053918b5521c181380a
sechmachine <groovykermit@outlook.com>
2024-10-10 23:24:44 +0700

    last modified by in training program might work now

--- src/main/java/org/fms/training/FmsApiApplication.java
+++ src/main/java/org/fms/training/FmsApiApplication.java
@@ -2,8 +2,10 @@ package org.fms.training;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditorAwareImpl")
 public class FmsApiApplication {
     public static void main(String[] args) {
         SpringApplication.run(FmsApiApplication.class, args);
