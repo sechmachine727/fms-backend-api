7789dd144767037ed9952d4fb6db0d1c7fdce5c3
DatHoang89 <datvip1938@gmail.com>
2024-10-21 09:41:14 +0700

    change file name

--- src/test/java/org/fms/training/service/impl/TopicImportServiceImplTest.java
+++ src/test/java/org/fms/training/service/impl/TopicImportServiceImplTest.java
@@ -27,10 +27,10 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
-class TopicImportServiceTest {
+class TopicTopicImportServiceTest {
 
     @InjectMocks
-    private TopicImportService topicImportService;
+    private TopicTopicImportService topicImportService;
 
     @Mock
     private TopicRepository topicRepository;
